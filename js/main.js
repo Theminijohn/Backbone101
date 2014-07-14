4159727238d78131bbdeb069a958c8827fe83170
The Mini John <admin@phoenix-prime.gr>
2014-07-14 19:57:48 +0300

    Validating Models

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 var Person = Backbone.Model.extend({
+
+  // The default values
   defaults: {
     name: 'The Mini John',
     age: 23,
     occupation: 'Worker'
   },
 
+  // Validation (Will trigger when setting an attribute)
+  validate: function(attrs) {
+    if (attrs.age < 0) {
+      return 'Age must be positive, stupid!';
+    }
+
+    if (!attrs.name) {
+      return 'Every Person must have a Name, fool!';
+    }
+  },
+
+  // Adding a Function
   work: function() {
     return this.get('name') + ' is working.'
   }
