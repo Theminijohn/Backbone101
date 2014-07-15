251dc81a4d433b2eecfcf0262b2f8f8e33b07a0a
The Mini John <admin@phoenix-prime.gr>
2014-07-15 11:59:48 +0300

    Wildcards and Splats

--- js/main.js
+++ js/main.js
@@ -10,16 +10,32 @@
   App.Router = Backbone.Router.extend({
     routes: {
       '': 'index',
-      'show': 'show'
+      'show/:id': 'show',
+      'download/:id*filename': 'download',
+      'search/:query': 'search',
+      '*other': 'default'
     },
 
     index: function() {
       console.log('Hi there from the index')
     },
 
-    show: function(){
-      console.log('Hi there from the Show action')
+    show: function(id){
+      console.log('Show route for id of ' + id)
+    },
+
+    download: function(filename){
+      console.log(filename)
+    },
+
+    search: function(query) {
+      console.log(query)
+    },
+
+    default: function(other) {
+      alert('Hmmm... not sure waht you need here? You accessed to: ' + other)
     }
+
   });
 
   new App.Router;
