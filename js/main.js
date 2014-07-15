73995eb97e5db2275121d5ae0a7a32eb43a12ab5
The Mini John <admin@phoenix-prime.gr>
2014-07-15 10:09:32 +0300

    Listening to Events

--- js/main.js
+++ js/main.js
@@ -12,8 +12,37 @@
 
   App.Models.Task = Backbone.Model.extend({});
 
+  App.Collections.Tasks = Backbone.Collection.extend({
+    model: App.Models.Task
+  });
+
+  App.Views.Tasks = Backbone.View.extend({
+    tagName: 'ul',
+
+    render: function() {
+      this.collection.each(this.addOne, this);
+      return this;
+    },
+
+    addOne: function(task) {
+      // creating a new child view
+      var taskView = new App.Views.Task({ model: task });
+
+      // append to the root element
+      this.$el.append(taskView.render().el);
+    }
+  });
+
   App.Views.Task = Backbone.View.extend({
     tagName: 'li',
+
+    events: {
+      'click': 'showAlert'
+    },
+
+    showAlert: function() {
+      alert('you clicked me!');
+    },
 
     render: function() {
       this.$el.html( this.model.get('title') );
@@ -21,14 +50,22 @@
     }
   });
 
-  var task = new App.Models.Task({
-    title: 'Go to the store',
-    priority: 4
-  });
+  var tasksCollection = new App.Collections.Tasks([
+    {
+      title: 'Go to the store',
+      priority: 4
+    },
+    {
+      title: 'Go to the mall',
+      priority: 3
+    },
+    {
+      title: 'Get to work',
+      priority: 5
+    }
+  ]);
 
-  var taskView = new App.Views.Task({ model: task });
-
-  console.log(taskView.render().el);
-
+  var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+  $('.tasks').html(tasksView.render().el)
 
 })();
